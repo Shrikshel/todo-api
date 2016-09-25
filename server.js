f1330c4c2a60b7265802216548c1ec56c7e93c47
Shrikshel <devshrikshel@gmail.com>
2016-09-25 19:49:48 +0530

    Port Modified

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 var app = express();
-var PORT = process.env.port || 3000;
+var PORT = process.env.PORT || 3000;
 
 app.get('/', function (req,res) {
   res.send('Yuppie! You are live on - TODO');
