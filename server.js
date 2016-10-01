aedbfb2766e1e11e0f1f6df145722a20b8e91255
Shrikshel <devshrikshel@gmail.com>
2016-10-01 14:31:54 +0530

    Converted GET /todos/:id to use sequelize

--- server.js
+++ server.js
@@ -44,13 +44,16 @@ app.get('/todos', function (req, res) {
 //GET /todos/:id
 app.get('/todos/:id',function (req,res) {
   var todoId = parseInt(req.params.id,10);
-  var matchedTodo = _.findWhere(todos, { id: todoId});
 
-  if(matchedTodo){
-    res.json(matchedTodo);
-  }else{
-    res.status(404).send();
-  }
+  db.todo.findById(todoId).then(function(todo){
+    if(!!todo){
+      res.json(todo.toJSON());
+    }else{
+      res.status(404).send();
+    }
+  }, function(e){
+    res.status(500).send();
+  });
 });
 
 //POST /todos
@@ -62,18 +65,6 @@ app.post('/todos', function(req,res){
   }, function(e){
     res.status(400).json(e);
   });
-
-  // if (! _.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
-  //   return res.status(404).send();
-  // }
-  //
-  // body.description = body.description.trim();
-  //
-  // body.id = todoID;
-  // todos.push(body);
-  // todoID++;
-  //
-  // res.send(body);
 });
 
 //PUT /todos/:id
@@ -116,7 +107,7 @@ app.delete('/todos/:id', function(req,res){
   }
 });
 
-db.sequelize.sync({force:true}).then(function(){
+db.sequelize.sync().then(function(){
   //Server Config
   app.listen(PORT, function () {
     console.log('Running on PORT: ' + PORT);
