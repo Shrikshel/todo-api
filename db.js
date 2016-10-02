0c80054de9e5254f5ddf895be9f7f419650fbe5f
Shrikshel <devshrikshel@gmail.com>
2016-10-03 02:06:18 +0530

    Added Postgres for Heroku

--- db.js
+++ db.js
@@ -1,8 +1,17 @@
 const Sequelize = require('sequelize');
-var sequelize = new Sequelize(undefined, undefined, undefined, {
-  'dialect': 'sqlite',
-  'storage': __dirname + '/data/play.sqlite'
-});
+var env = process.env.NODE_ENV || 'development';
+var sequelize;
+
+if (env === 'production') {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    'dialect':'postgres'
+  });
+} else {
+  sequelize = new Sequelize(undefined, undefined, undefined, {
+    'dialect': 'sqlite',
+    'storage': __dirname + '/data/play.sqlite'
+  });
+}
 
 var db = {}
 db.todo = sequelize.import(__dirname + '/models/todo.js');
